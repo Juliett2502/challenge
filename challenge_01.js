b07dc3e85788f50688e5a5d32256354becd10a1d
Andrei <popovavppwd@gmail.com>
2020-07-08 00:17:35 +0200

    Summe rechnen

--- challenge_01.js
+++ challenge_01.js
@@ -8,4 +8,11 @@ Berechnen Sie die Summe der Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 ‘Die Summe der Zahlen ist: summe ‘
 */
-var Zahl
+
+
+var Zahl1Str= prompt ("Geben Sie erste Zahl: ");
+var Zahl1 = parseInt (Zahl1Str);
+var Zahl2Str = prompt ("Geben Sie zweite Zahl: ");
+var Zahl2 = parseInt (Zahl2Str);
+var summe = Zahl1+Zahl2;
+alert ("Die Summe der Zahlen ist: " + summe);
